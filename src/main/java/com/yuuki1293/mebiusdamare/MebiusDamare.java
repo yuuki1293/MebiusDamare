24645b78a7af285bf1ab1b814c8577c9981ac96d
yuuki1293 <yuuki1293@outlook.jp>
2024-11-11 16:10:53 +0900

    rename modids

--- src/main/java/com/yuuki1293/mebiusdamare/MebiusDamare.java
+++ src/main/java/com/yuuki1293/mebiusdamare/MebiusDamare.java
@@ -1,13 +1,12 @@
-package com.example.modid;
+package com.yuuki1293.mebiusdamare;
 
-import com.example.modid.Tags;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class MebiusDamare {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
